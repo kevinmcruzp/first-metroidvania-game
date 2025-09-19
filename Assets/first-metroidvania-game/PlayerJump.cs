eaf532da672c5708e91799e39be4d167c4394576
Kevin Cruz <kevin_mcp@outlook.com>
2025-09-18 22:47:32 -0300

    feat: allow continuous jump while holding Jump button

--- Assets/first-metroidvania-game/PlayerJump.cs
+++ Assets/first-metroidvania-game/PlayerJump.cs
@@ -12,16 +12,21 @@ public class PlayerJump : MonoBehaviour
     // rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
     [Header("Public Vars")]
-    public float jumpForce = 3.0f;
+    public float jumpForce = 3.5f;
+    public float jumpHoldForce = 0.3f;
     public bool grounded;
     private Rigidbody2D rb2d;
+    public float jumpTime = 0.25f;
+    private float jumpTimeCounter;
+    private bool stoppingJump;
+    private bool isJumpPressed;
 
     private PlayerInput playerInput;
     private InputAction jumpAction;
 
     [Header("Private Vars")]
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float radius = 0.03f;
+    [SerializeField] private float radius = 0.05f;
     [SerializeField] private LayerMask groundLayer;
 
     void Start()
@@ -30,20 +35,52 @@ public class PlayerJump : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         jumpAction = playerInput.actions["Jump"];
 
-        // Subscribe to the 'performed' event of the Jump action
+        // Subscribe to the jump action events
         jumpAction.performed += OnJumpPerformed;
+        jumpAction.canceled += OnJumpCanceled;
     }
 
     void OnJumpPerformed(InputAction.CallbackContext context)
     {
-        // This method will be called when the Jump action is performed (e.g., Spacebar is pressed)
+        isJumpPressed = true;
         Debug.Log("Jump action performed!");
-        // Add your jump logic here (e.g., applying force to a Rigidbody)
+    }
+
+    void OnJumpCanceled(InputAction.CallbackContext context)
+    {
+        isJumpPressed = false;
+        jumpTimeCounter = 0;
+        stoppingJump = true;
+        Debug.Log("Jump action canceled!");
+    }
+
+    void Update()
+    {
         grounded = Physics2D.OverlapCircle(groundCheck.position, radius, groundLayer);
-        if (grounded)
+
+        // Start jump if grounded and jump is pressed
+        if (isJumpPressed && grounded && jumpTimeCounter <= 0)
         {
-            // rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpForce);
-            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpTimeCounter = jumpTime;
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpForce);
+            stoppingJump = false;
+        }
+
+        // Continue jump while button is held (variable jump height)
+        if (isJumpPressed && !stoppingJump && jumpTimeCounter > 0 && !grounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpHoldForce, ForceMode2D.Force);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        else if (!isJumpPressed && jumpTimeCounter > 0)
+        {
+            // Cut jump short if button released
+            jumpTimeCounter = 0;
+            stoppingJump = true;
+            if (rb2d.linearVelocity.y > 0)
+            {
+                rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, rb2d.linearVelocity.y * 0.25f);
+            }
         }
     }
 
@@ -52,6 +89,7 @@ public class PlayerJump : MonoBehaviour
         Debug.Log("Jump action unsubscribed.");
         // Unsubscribe to prevent memory leaks when the object is destroyed
         jumpAction.performed -= OnJumpPerformed;
+        jumpAction.canceled -= OnJumpCanceled;
     }
 
     private void OnDrawGizmos()
